0528966dd3af46ac82cbe1afd2bc0d2d708a804a
Anna Fedorova <anna.fedorova.testing@gmail.com>
2026-06-18 15:54:22 +0300

    Фикстуры Pytest

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,29 +1,23 @@
 from http import HTTPStatus
 
-from clients.authentication.authentication_client import get_authentication_client
+import pytest
+
+from clients.authentication.authentication_client import AuthenticationClient
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
-from clients.users.public_users_client import get_public_users_client
-from clients.users.users_schema import CreateUserRequestSchema
+from tests.conftest import UserFixture
 from tools.assertions.authentification import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
 
-def test_success_login():
-    public_users_client = get_public_users_client()
-    authentification_client = get_authentication_client()
+@pytest.mark.regression
+@pytest.mark.authentication
+def test_login(function_user: UserFixture, authentification_client: AuthenticationClient):
+    request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+    response = authentification_client.login_api(request)
+    response_data = LoginResponseSchema.model_validate_json(response.text)
 
-    create_user_request = CreateUserRequestSchema()
-    public_users_client.create_user(create_user_request)
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_login_response(response_data)
 
-    login_request = LoginRequestSchema(
-        email=create_user_request.email,
-        password=create_user_request.password
-    )
-    login_response = authentification_client.login_api(login_request)
-    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
-
-    assert_status_code(login_response.status_code, HTTPStatus.OK)
-    assert_login_response(login_response_data)
-
-    validate_json_schema(login_response.json(), login_response_data.model_json_schema())
+    validate_json_schema(response.json(), response_data.model_json_schema())
